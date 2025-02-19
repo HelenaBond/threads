44b41dcd1ccebd8df87748952f1fb8ae6280aec0
Elena <bond.elena.15.05@gmail.com>
2025-02-20 00:12:10 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/org/example/concurrent/ConsoleProgress.java
@@ -0,0 +1,25 @@
+package org.example.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[] {'-', '\\', '|', '/'};
+        int index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + process[index]);
+            index = (index + 1) % process.length;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}
