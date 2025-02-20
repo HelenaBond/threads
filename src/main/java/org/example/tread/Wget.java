eecf048b1d43bf98c80c6ac80fd8c49b25ada3a2
Elena <bond.elena.15.05@gmail.com>
2025-02-20 22:04:45 +0300

    8. Скачивание файла с ограничением. [#144271]

---
+++ src/main/java/org/example/tread/Wget.java
@@ -0,0 +1,50 @@
+package org.example.tread;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        var startAt = System.currentTimeMillis();
+        var file = new File("tmp.xml");
+        try (var input = new URL(url).openStream();
+             var output = new FileOutputStream(file)) {
+            System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
+            var dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = input.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                var downloadAt = System.nanoTime();
+                output.write(dataBuffer, 0, bytesRead);
+                var time = System.nanoTime() - downloadAt;
+                System.out.println("Read 1024 bytes : " + time + " nano.");
+                int currSpeed = (1024_000_000) / (int) time;
+                int pause = currSpeed / speed;
+                System.out.println(pause);
+                Thread.sleep(pause);
+            }
+            System.out.println(Files.size(file.toPath()) + " bytes");
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+    }
+}
