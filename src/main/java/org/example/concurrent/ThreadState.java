661518396b7885c8c29b16507de817635499783d
Elena <bond.elena.15.05@gmail.com>
2025-02-21 11:26:37 +0300

    Fix 3. Состояние нити. [#229175 #533178]

--- src/main/java/org/example/concurrent/ThreadState.java
+++ src/main/java/org/example/concurrent/ThreadState.java
@@ -3,18 +3,17 @@ package org.example.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(() ->
-                System.out.println("First thread finished"));
+                System.out.println("Thread finished " + Thread.currentThread().getName()));
 
         Thread second = new Thread(() ->
-                System.out.println("Second thread finished"));
+                System.out.println("Thread finished " + Thread.currentThread().getName()));
 
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getState());
-        }
-        while (second.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getState());
+        while (first.getState() != Thread.State.TERMINATED
+            || second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState() + " " + first.getName());
+            System.out.println(first.getState() + " " + second.getName());
         }
         System.out.println("All threads finished!");
     }
