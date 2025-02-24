e5605453de95d27f3eb35da785511db25c8a00a1
Elena <bond.elena.15.05@gmail.com>
2025-02-24 10:47:39 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/org/example/tread/UserCache.java
@@ -0,0 +1,28 @@
+package org.example.tread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> result = new ArrayList<>();
+        for (Map.Entry<Integer, User> userCache : users.entrySet()) {
+            result.add(findById(userCache.getKey()));
+        }
+        return result;
+    }
+}
